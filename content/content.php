1398c3bb73cd8b0e4baf5a0de6fc338743a8cf90
Gaurav Pareek <me@grv.rocks>
2016-01-06 23:00:27 +0530

    Moved byline above entry-title

--- content/content.php
+++ content/content.php
@@ -29,14 +29,14 @@
 
 		<header class="entry-header">
 
-			<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . get_permalink() . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
-
 			<div class="entry-byline">
 				<span <?php hybrid_attr( 'entry-author' ); ?>><?php the_author_posts_link(); ?></span>
 				<time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
 				<?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
 				<?php edit_post_link(); ?>
 			</div><!-- .entry-byline -->
+			
+			<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . get_permalink() . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
 
 		</header><!-- .entry-header -->
 
